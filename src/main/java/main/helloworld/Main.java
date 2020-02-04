5c5ed8cfda9e1f0d5b12cda26efff36a4c484422
Bojana Didic Laketic <bojana.didic.laketic@gmail.com>
2020-02-04 18:45:39 +0100

    Initial commit

---
+++ src/main/java/main/helloworld/Main.java
@@ -0,0 +1,19 @@
+package main.helloworld;
+
+/**
+ *
+ * @author qa
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+        Sustem.out.println*("Bojana Didic Laketic")
+        
+    }
+    
+}
