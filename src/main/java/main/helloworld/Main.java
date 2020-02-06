d9c84002f9524f73a24110c226375e890d518517
Bojana Didic Laketic <bojana.didic.laketic@gmail.com>
2020-02-06 20:04:01 +0100

    adds examples for different loop types

--- src/main/java/main/helloworld/Main.java
+++ src/main/java/main/helloworld/Main.java
@@ -13,49 +13,133 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        System.out.println("Hello World");
-        System.out.println("Bojana");
+//        System.out.println("Hello World");
+//        System.out.println("Bojana");
+//        
+//        int age = 5;
+//       
+//
+//        System.out.print("Unesite broj godina: ");
+//        Scanner input = new Scanner(System.in);
+//        int userAge = input.nextInt();
+//        
+//        System.out.println("Korisnik je uneo: " + userAge);
+//        
+//         if (userAge >= 18) {
+//  
+//            System.out.println("Osoba je punoletna");
+//        } else {
+//            System.out.println("Osoba je maloletna");
+//        }
+//         
+//         
+//         
+//         
+//         
+//         System.out.print("Unesite broj godina: ");
+//        input = new Scanner(System.in);
+//        int personAge = input.nextInt();
+//        
+//        System.out.println("Korisnik je uneo: " + userAge);
+//        
+//         if (personAge > 0 && personAge <= 5) {
+//            System.out.println("Baby");
+//        } else if (personAge <= 11) {
+//            System.out.println("Kid");
+//        } else if (personAge <= 17) { 
+//            System.out.println("Teen");
+//        } else if (personAge >= 18) {
+//            System.out.println("Adult");
+//        } else {
+//            System.out.println("Invalid");
+//        }
+//        
+//        
+//         
+//         
+//         
+//         System.out.print("Unesiti redni broj dana u nedelji (1-7): ");
+//        Scanner input3 = new Scanner(System.in);
+//        int dayNumber = input3.nextInt();
+//        String day;
+//        switch (dayNumber) {
+//           case 1:
+//             day = "Ponedeljak";
+//             break;
+//           case 2:
+//             day = "Utorak";
+//             break;
+//           case 3:
+//             day = "Sreda";
+//             break;
+//           case 4:
+//             day = "Četvrtak";
+//             break;
+//           case 5:
+//             day = "Petak";
+//             break;
+//           case 6:
+//             day = "Subota";
+//             break;
+//           case 7:
+//             day = "Nedelja";
+//             break;
+//           default:
+//             day = "Netačan unos";
+//             break;
+//        }
+//        System.out.println(day);
         
-        int age = 5;
-       
-
-        System.out.print("Unesite broj godina: ");
-        Scanner input = new Scanner(System.in);
-        int userAge = input.nextInt();
-        
-        System.out.println("Korisnik je uneo: " + userAge);
-        
-         if (userAge >= 18) {
-  
-            System.out.println("Osoba je punoletna");
-        } else {
-            System.out.println("Osoba je maloletna");
-        }
-         
-         
-         
-         
-         
-         System.out.print("Unesite broj godina: ");
-        input = new Scanner(System.in);
-        int personAge = input.nextInt();
-        
-        System.out.println("Korisnik je uneo: " + userAge);
-        
-         if (personAge > 0 && personAge <= 5) {
-            System.out.println("Baby");
-        } else if (personAge <= 11) {
-            System.out.println("Kid");
-        } else if (personAge <= 17) { 
-            System.out.println("Teen");
-        } else if (personAge >= 18) {
-            System.out.println("Adult");
-        } else {
-            System.out.println("Invalid");
-        }
         
         
-    }
+        
+//        System.out.println("While loop");
+//        int x =5;
+//        while(x < 10) {
+//            System.out.println(x);
+//            x = x + 1;
+//        }  // x++;
+        
+        
+//        System.out.println("While loop");
+//        int x = 10;
+//        while(x > 0) {
+//            System.out.println(x);
+//            x = x - 1;
+//        }
+            // x--;   
+            
+//           System.out.println("While loop");
+//           int x = 20;
+//           do {
+//             System.out.println(x); 
+//             x--;
+//             
+//           } while(x > 15);
+           
+           
+           
+//           System.out.println("For loop");
+//           for (int i = 50; i < 60; i++) {
+//               System.out.println(i); 
+//           }
+           
+           
+            
+//             System.out.println("For loop");
+//             for (int i = 30; i >= 20; i--) {
+//                System.out.println(i); 
+//           }
+            
+             
+             System.out.println("Koordinate 1: ");
+             for (int i = 0; i < 10; i++) {
+                 for (int j = 0; j < 10; j++) {
+                 System.out.print("(" + i + "," + j + ") "); 
+           }
+             
+             System.out.println();
+   }
     
         
         
@@ -64,4 +148,4 @@ public class Main {
 }
 
 
-    
+}
